276bbb2a08e722bba968d0f5307d0ed31e8b9d43
IKarbowiak <iga.karbowiak@saleor.io>
2023-10-09 14:27:41 +0200

    Use voucher and voucher_code from checkout_info

--- saleor/graphql/checkout/dataloaders.py
+++ saleor/graphql/checkout/dataloaders.py
@@ -35,6 +35,7 @@ from ..discount.dataloaders import (
     PromotionByRuleIdLoader,
     PromotionRuleByIdLoader,
     VoucherByCodeLoader,
+    VoucherCodeByCodeLoader,
     VoucherInfoByVoucherCodeLoader,
 )
 from ..plugins.dataloaders import get_plugin_manager_promise
@@ -467,6 +468,9 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                     if checkout.voucher_code
                 }
                 vouchers = VoucherByCodeLoader(self.context).load_many(voucher_codes)
+                voucher_codes = VoucherCodeByCodeLoader(self.context).load_many(
+                    voucher_codes
+                )
                 channel_ids = [channel.id for channel in channels]
                 tax_configurations = TaxConfigurationByChannelId(
                     self.context
@@ -480,6 +484,7 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                         listings_for_channels,
                         collection_points,
                         vouchers,
+                        voucher_codes,
                         tax_configurations,
                     ) = results
                     address_map = {address.id: address for address in addresses}
@@ -493,6 +498,7 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                         for collection_point in collection_points
                     }
                     voucher_map = {voucher.code: voucher for voucher in vouchers}
+                    voucher_code_map = {code.code: code for code in voucher_codes}
                     tax_configuration_by_channel_map = {
                         tax_configuration.channel_id: tax_configuration
                         for tax_configuration in tax_configurations
@@ -513,6 +519,7 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                             None, shipping_address
                         )
                         voucher = voucher_map.get(checkout.voucher_code)
+                        voucher_code = voucher_code_map.get(checkout.voucher_code)
                         checkout_info = CheckoutInfo(
                             checkout=checkout,
                             user=user_map.get(checkout.user_id),
@@ -530,6 +537,7 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                             valid_pick_up_points=[],
                             all_shipping_methods=[],
                             voucher=voucher,
+                            voucher_code=voucher_code,
                         )
                         shipping_method_listings = [
                             listing
@@ -558,6 +566,7 @@ class CheckoutInfoByCheckoutTokenLoader(DataLoader[str, CheckoutInfo]):
                         shipping_method_channel_listings,
                         collection_points,
                         vouchers,
+                        voucher_codes,
                         tax_configurations,
                     ]
                 ).then(with_checkout_info)
